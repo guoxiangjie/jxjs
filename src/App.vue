dddb1e81af61bad7dbaeca098eecdfff9c2b2406
gxj <1055433631@qq.com>
2019-06-19 18:07:40 +0800

    test

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,81 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" onselectstart="return false;">
+    <mu-container class>
+      <mu-row>
+        <mu-col span="12">
+          <mu-appbar style="width: 100%;" color="primary">
+            绩效计算工具
+            <mu-button flat slot="right">{{data | datrfmt('HH:mm:ss')}}</mu-button>
+          </mu-appbar>
+        </mu-col>
+        <mu-col span="12">
+          <mu-card>
+            <mu-card-header title="首先，需要确定一下配置项，这很重要！">
+              <mu-icon style="margin-right:10px" slot="avatar" value="info" color="red"></mu-icon>
+            </mu-card-header>
+          </mu-card>
+          <mu-card class="action">
+            <mu-row>
+              <mu-col span="6">
+                <mu-row align-items="center">
+                  <mu-col style="color:#999" span="2">当前月份</mu-col>
+                  <mu-col style="color:#2196f3" span="6">
+                    <mu-date-input
+                      icon="today"
+                      v-model="value10"
+                      label="选择月份"
+                      type="month"
+                      label-float
+                      full-width
+                    ></mu-date-input>
+                  </mu-col>
+                </mu-row>
+              </mu-col>
+              <mu-col span="6">
+                <mu-row align-items="center">
+                  <mu-col style="color:#999" span="2">工作日</mu-col>
+                  <mu-col style="color:#2196f3" span="6">
+                    <mu-date-input v-model="value2" label="总工作天数" label-float full-width></mu-date-input>
+                  </mu-col>
+                </mu-row>
+              </mu-col>
+            </mu-row>
+          </mu-card>
+        </mu-col>
+      </mu-row>
+    </mu-container>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import { setInterval } from "timers";
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
-}
+  name: "app",
+  data() {
+    return {
+      data: new Date()
+    };
+  },
+  created() {
+    this.updataDate();
+  },
+
+  methods: {
+    updataDate() {
+      setInterval(() => {
+        this.data = new Date();
+      }, 1000);
+    }
+  },
+  components: {}
+};
 </script>
 <style lang="stylus">
-#app
-  font-family 'Avenir', Helvetica, Arial, sans-serif
-  -webkit-font-smoothing antialiased
-  -moz-osx-font-smoothing grayscale
-  text-align center
-  color #2c3e50
-  margin-top 60px
+body {
+  background: #f2f2f2;
+
+  .action {
+    padding: 20px;
+  }
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,16 @@
 import Vue from 'vue'
 import App from './App.vue'
+import MuseUI from 'muse-ui';
+import 'muse-ui/dist/muse-ui.css';
+import moment from 'moment'
+import './assets/material-icons.css'
 
 Vue.config.productionTip = false
+Vue.use(MuseUI);
 
+Vue.filter('datrfmt', function (input, fmtstring) {
+  return moment(input).format(fmtstring);
+})
 new Vue({
   render: h => h(App),
 }).$mount('#app')
